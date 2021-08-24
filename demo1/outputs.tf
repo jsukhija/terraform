699d56f9463fbe74294b2acefbfda33d9fd1245e
jsukhija <89379533+jsukhija@users.noreply.github.com>
2021-08-24 09:59:44 +0530

    Update outputs.tf

--- demo1/outputs.tf
+++ demo1/outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_bucket.test_bucket.name
+  value       = aws_s3_bucket.test_bucket.name
 }
 
 output "acl" {
   description = "Access type"
-  value       = aws_bucket.test_bucket.acl
+  value       = aws_s3_bucket.test_bucket.acl
 }
