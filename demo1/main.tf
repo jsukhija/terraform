2dfebe44ca2aa1c0393ff1aecaa606989fb0b3ff
jsukhija <89379533+jsukhija@users.noreply.github.com>
2021-08-24 09:47:31 +0530

    Create main.tf

---
+++ demo1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "tf_test_bucket"
+  acl    = "public"
+
+  tags = {
+    Name        = "Jishant Bucket"
+  }
+}
+
+
