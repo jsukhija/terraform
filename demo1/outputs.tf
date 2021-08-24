ee86ecfc04af1026384f1e7e1a052d08f6177a63
jsukhija <89379533+jsukhija@users.noreply.github.com>
2021-08-24 10:00:40 +0530

    Update outputs.tf

--- demo1/outputs.tf
+++ demo1/outputs.tf
@@ -1,6 +1,6 @@
 output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.test_bucket.name
+  description = "Bucket IDt"
+  value       = aws_s3_bucket.test_bucket.id
 }
 
 output "acl" {
